00efb90bdc319ee4d76987da9211811c0cca50dd
Karolina <karton@st.amu.edu.pl>
2020-10-23 11:22:45 +0200

    wiadomosc

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+        Logger log = Logger.getLogger(HelloWorld.class);
         System.out.println("Hello World");
+        log.info("Moja wiadomosc");
     }
 }
